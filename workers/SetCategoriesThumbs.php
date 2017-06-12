016445f5976fe879afaf6d926ce3e5e16fefa9d4
ytubes <adx.informer@gmail.com>
2017-06-13 01:00:17 +0700

    Create SetCategoriesThumbs.php

--- workers/SetCategoriesThumbs.php
+++ workers/SetCategoriesThumbs.php
@@ -8,104 +8,104 @@ use \yii\db\Expression;
 use ytubes\admin\videos\models\VideosStats;
 use ytubes\admin\videos\models\VideosCategories;
 use ytubes\admin\videos\models\VideosImages;
-use frontend\models\videos\repositories\VideosRepository;
+use ytubes\admin\videos\models\repositories\VideosRepository;
 
 /**
  * RecalculateCTR represents the model behind the search form about `frontend\models\videos\Videos`.
  */
 class SetCategoriesThumbs extends \yii\base\Object //implements Task\Handler\TaskHandlerInterface
 {
-	private $db;
-	private $params;
+    private $db;
+    private $params;
 
-	private $errors = [];
+    private $errors = [];
 
-	public function __construct($config = [])
-	{
-		$this->db = Yii::$app->db;
-		$this->params = Yii::$app->params['videos'];
+    public function __construct($config = [])
+    {
+        $this->db = Yii::$app->db;
+        $this->params = Yii::$app->params['videos'];
 
-		parent::__construct($config);
-	}
-
-	public function handle()
-	{
-		$this->setCategoriesThumbs();
+        parent::__construct($config);
     }
 
-	/**
-	 *	Устанавливает главную тумбу категории
-	 */
+    public function handle()
+    {
+        $this->setCategoriesThumbs();
+    }
+
+    /**
+     * Устанавливает главную тумбу категории
+     */
     public function setCategoriesThumbs()
     {
-		$categories = VideosCategories::find()
-        	->all();
+        $categories = VideosCategories::find()
+            ->all();
 
         if (empty($categories)) {
-        	return;
+            return;
         }
 
-			//SELECT `image_id` FROM `videos_stats` WHERE (`category_id`=20) AND (`best_image`=1) AND `image_id` NOT IN (1,2,3) ORDER BY `ctr` LIMIT 1
+            //SELECT `image_id` FROM `videos_stats` WHERE (`category_id`=20) AND (`best_image`=1) AND `image_id` NOT IN (1,2,3) ORDER BY `ctr` LIMIT 1
         try {
-	        $usedImagesIds = [];
+            $usedImagesIds = [];
 
-	        $searchModel = new VideosRepository();
+            $searchModel = new VideosRepository();
 
-	        foreach ($categories as $category) {
+            foreach ($categories as $category) {
 
-					// Выбрать тумбы с первой страницы категории
-				$items = $searchModel->getItemsFromCategory([$searchModel->formName() => ['slug' => $category->slug, 'sort' => 'popular']], $category);
+                    // Выбрать тумбы с первой страницы категории
+                $items = $searchModel->getItemsFromCategory([$searchModel->formName() => ['slug' => $category->slug, 'sort' => 'popular']], $category);
 
-				$imagesIds = [];
+                $imagesIds = [];
 
-				if (empty($items)) {
-					continue;
-				}
+                if (empty($items)) {
+                    continue;
+                }
 
-				foreach ($items as $item) {
-					$imagesIds[] = $item->image->image_id;
-				}
+                foreach ($items as $item) {
+                    $imagesIds[] = $item->image->image_id;
+                }
 
-					// Отсеять уже использованные в других категориях (уникальные должны быть)
-				$unusedIds = array_diff($imagesIds, $usedImagesIds);
+                    // Отсеять уже использованные в других категориях (уникальные должны быть)
+                $unusedIds = array_diff($imagesIds, $usedImagesIds);
 
-					// Если уникальные иды остались, то выбрать первую и установить ее как обложку категории.
-				if (!empty($unusedIds)) {
-					$firstId = array_shift($unusedIds);
-					$img = VideosImages::findOne($firstId);
+                    // Если уникальные иды остались, то выбрать первую и установить ее как обложку категории.
+                if (!empty($unusedIds)) {
+                    $firstId = array_shift($unusedIds);
+                    $img = VideosImages::findOne($firstId);
 
-					if (null !== $img) {
-						$category->image = $img->filepath;
-						$category->save(true);
-					}
+                    if (null !== $img) {
+                        $category->image = $img->filepath;
+                        $category->save(true);
+                    }
 
-						// Записать, что данная тумба уже используется.
-					$usedImagesIds[] = $img->image_id;
-				}
-	        }
+                        // Записать, что данная тумба уже используется.
+                    $usedImagesIds[] = $img->image_id;
+                }
+            }
 
-		} catch(\Exception $e) {
-		    $transaction->rollBack();
-		    throw $e;
-		} catch(\Throwable $e) {
-		    $transaction->rollBack();
-		}
+        } catch(\Exception $e) {
+            //$transaction->rollBack();
+            throw $e;
+        } catch(\Throwable $e) {
+            //$transaction->rollBack();
+        }
 
     }
 
     /**
      * {@inheritdoc}
      */
-	public function getErrors()
-	{
-		return $this->errors;
-	}
+    public function getErrors()
+    {
+        return $this->errors;
+    }
 
     /**
      * {@inheritdoc}
      */
-	public function hasErrors()
-	{
-		return (!empty($this->errors));
-	}
+    public function hasErrors()
+    {
+        return (!empty($this->errors));
+    }
 }
