51d82a547abdca8f19bd055f726337edb7683cf0
ytubes <adx.informer@gmail.com>
2017-10-04 20:04:45 +0700

    Hot fix

--- migrations/m171004_101411_category_enabled.php
+++ migrations/m171004_101411_category_enabled.php
@@ -16,7 +16,7 @@ class m171004_101411_category_enabled extends Migration
 
 		if ($tableSchema !== null) {
 	    	$this->execute("ALTER TABLE `{$tableName}` ADD `enabled` TINYINT UNSIGNED NOT NULL DEFAULT '0' AFTER `reset_clicks_period`");
-	    	$this->execute("UPDATE `{$tableName}` SET `enbled`=1");
+	    	$this->execute("UPDATE `{$tableName}` SET `enabled`=1");
 	    }
     }
 
